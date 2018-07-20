44e9bd5e80197f246ba5202324be4016b56bd49d
Jim <jim@advancebass.com>
2018-07-19 20:53:00 -0700

    tidy single.php

--- page-templates/single.php
+++ page-templates/single.php
@@ -8,8 +8,8 @@ while ( have_posts() ) : the_post();
 	$member_img         = get_post_meta( get_the_ID(), '_' . BCW_MEMBERS_POST_TYPE . '_img', true );
 	$member_description = get_post_meta( get_the_ID(), '_' . BCW_MEMBERS_POST_TYPE . '_description', true );
 
-	the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-	echo '<h3>' . esc_html( $member_title ) . '</h3>';
+	the_title( sprintf( '<h1 class="entry-title">', esc_url( get_permalink() ) ), '</h1>' );
+	echo '<h2>' . esc_html( $member_title ) . '</h2>';
 	echo "<img alt='" . $member_title . "' src='" . esc_url_raw( $member_img ) . "'/>";
 	echo esc_html( $member_description );
 
